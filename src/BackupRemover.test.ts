094896724c2128148ed095a6cbd85478a82b67a7
Konstantin Lazukin <konstantin@enflow.io>
2023-09-09 17:12:20 +0300

    tests list

--- src/BackupRemover.test.ts
+++ src/BackupRemover.test.ts
@@ -1,4 +1,25 @@
 
-test("adds 1 + 2 to equal 3", () => {
+test("doesnt remove fresh backups", () => {
+    expect(3).toBe(3);
+});
+
+
+test("removes old backups", () => {
+    expect(3).toBe(3);
+});
+
+test("removes doble copies of a day ", () => {
+    expect(3).toBe(3);
+});
+
+test("consoles that dates are not correct", () => {
+    expect(3).toBe(3);
+});
+
+test("consoles if cant delete file", () => {
+    expect(3).toBe(3);
+});
+
+test("consoles if cant delete file", () => {
     expect(3).toBe(3);
 });
